c521f381f1e80d91dc25c1422da4cdcf1c857098
dariasolodukha <darinka.solodka@icloud.com>
2026-04-30 13:20:40 +0300

    Delete gulpfile.js

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp');
-
-// задача копіювання файлів
-exports.cloneFiles = async () => {
-  return gulp.src('app/*.html')   // беремо всі html з app
-    .pipe(gulp.dest('public'));   // копіюємо в public
-}
